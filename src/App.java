dfd4a4e6139f84b42192350bc426c824fee82165
Aina Ponseti <aina.ponseti@estudiants.urv.cat>
2022-11-28 15:48:18 +0100

    Backtracking simple solution finished

--- src/App.java
+++ src/App.java
@@ -8,12 +8,12 @@ public class App {
 
     public static void main(String[] args) {
 
-        File file = new File("src/sudokuFiles/easy.txt");
-        List<Sudoku> sudokuList = readSudokuFromFile(file);
-        file = new File("src/sudokuFiles/easySolutions.txt");
-        List<Sudoku> sudokuSolutions = readSudokuFromFile(file);
+        File file1 = new File("src/sudokuFiles/easy.txt");
+        List<Sudoku> sudokuList = readSudokuFromFile(file1,0);
+        File file2 = new File("src/sudokuFiles/easySolutions.txt");
+        List<Sudoku> sudokuSolutions = readSudokuFromFile(file2,0);
 
-
+        System.out.println("\n--------------- Greedy ---------------\n");
         for (int i = 0; i < sudokuList.size(); i++){
             System.out.println("\n--------------- Sudoku #" + i + " ---------------\n");
             Sudoku sudoku = sudokuList.get(i);
@@ -21,10 +21,20 @@ public class App {
 
             System.out.println(sudoku);
         }
+
+        sudokuList = readSudokuFromFile(file1, 1);
+        sudokuSolutions = readSudokuFromFile(file2,1);
+
+        System.out.println("\n--------------- Backtracking ---------------\n");
+        for (int i = 0; i < sudokuList.size(); i++){
+            System.out.println("\n--------------- Sudoku #" + i + " ---------------\n");
+            Sudoku sudoku = sudokuList.get(i);
+            int result = sudoku.solve();
+            System.out.println(sudoku);
+        }
     }
 
-
-    public static List<Sudoku> readSudokuFromFile(File file){
+    public static List<Sudoku> readSudokuFromFile(File file,int type){
 
         List<Sudoku> sudokuList = new ArrayList<>();
         String[] data;
@@ -49,8 +59,13 @@ public class App {
                         j++;
                     }
                 }
-
-                Sudoku newSudoku = new GreedySudoku(board);
+                Sudoku newSudoku;
+                if (type == 0) {
+                    newSudoku = new GreedySudoku(board);
+                }
+                else {
+                    newSudoku = new BacktrackingSudoku(board);
+                }
                 sudokuList.add(newSudoku);
             }
 
--- tests/SudokuTests.java
+++ tests/SudokuTests.java
@@ -60,5 +60,69 @@ public class SudokuTests {
         return sudokuList;
     }
 
+    @Test
+    void backtrackingSolverEasyTests(){
+        File file2 = new File("src/sudokuFiles/easy.txt");
+        List<BacktrackingSudoku> sudokuList = readSudokuBtFromFile(file2);
+        file2 = new File("src/sudokuFiles/easySolutions.txt");
+        List<BacktrackingSudoku> sudokuSolutions = readSudokuBtFromFile(file2);
+
+        for (int i = 0; i < sudokuList.size(); i++){
+            sudokuList.get(i).solve();
+            Assert.assertEquals(sudokuSolutions.get(i).getSudoku(), sudokuList.get(i).getSolution());
+        }
+    }
+
+    @Test
+    void backtrackingSolverMediumTests(){
+        File file2 = new File("src/sudokuFiles/medium.txt");
+        List<BacktrackingSudoku> sudokuList = readSudokuBtFromFile(file2);
+        file2 = new File("src/sudokuFiles/mediumSolutions.txt");
+        List<BacktrackingSudoku> sudokuSolutions = readSudokuBtFromFile(file2);
+
+        for (int i = 0; i < sudokuList.size(); i++){
+            sudokuList.get(i).solve();
+            Assert.assertEquals(sudokuSolutions.get(i).getSudoku(), sudokuList.get(i).getSolution());
+        }
+    }
+    public static List<BacktrackingSudoku> readSudokuBtFromFile(File file){
+
+        List<BacktrackingSudoku> sudokuList = new ArrayList<>();
+        String[] data;
+        int j, k;
+
+
+        try {
+
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()){
+                int[][] board = new int[9][9];
+                j = 0;
+                k = 0;
+
+                data = fileReader.nextLine().split(",");
+                for (int i = 0; i < data.length; i++){
+
+                    board[j][k] = Integer.valueOf(data[i]);
+                    k++;
+                    if (k >= 9){
+                        k = 0;
+                        j++;
+                    }
+                }
+
+                BacktrackingSudoku newSudoku = new BacktrackingSudoku(board);
+                sudokuList.add(newSudoku);
+
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+        }
+
+        return sudokuList;
+    }
+
 
 }
+
